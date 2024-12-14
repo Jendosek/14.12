d13f4c0e00abd22b7e52c72257adc02ba88cc6df
Jendosek <bala01081977@gmail.com>
2024-12-14 13:10:27 +0200

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿namespace _14._12;
+﻿using System.Text;
+
+namespace _14._12;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.InputEncoding = Encoding.Unicode;  
+        Console.OutputEncoding = Encoding.Unicode;
+        
     }
 }
